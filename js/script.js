f4eb480611f4848c2cc87bf6adacfc47de22defe
Rodolfo <rodolfog.peixoto@gmail.com>
2017-08-15 04:15:00 -0300

    Added style and created a loop for apply the style in all the tr

--- js/script.js
+++ js/script.js
@@ -2,32 +2,35 @@ var title       = document.querySelector(".title");
 title.textContent = 'ROGPEnutri';
 
 function calcIMC(){
-    var paciente         = document.querySelector("#primeiro-paciente");
-    var tdPeso           = paciente.querySelector(".info-peso");
-    var tdAltura         = paciente.querySelector(".info-altura");
-    var tdIMC            = paciente.querySelector(".info-imc");
-    var peso             = tdPeso.textContent;
-    var altura           = tdAltura.textContent;
-    if(this.validationPeso(peso, tdIMC) !== false && this.validationAltura(altura, tdIMC) !== false){
-      var imc              = peso / (Math.pow(altura,2));
-      tdIMC.textContent    = imc;
+    var pacientes         = document.querySelectorAll(".paciente");
+    for( var i = 0; i < pacientes.length; i++){
+        var tdPeso            = pacientes[i].querySelector(".info-peso");
+        var tdAltura          = pacientes[i].querySelector(".info-altura");
+        var tdIMC             = pacientes[i].querySelector(".info-imc");
+        var peso              = tdPeso.textContent;
+        var altura            = tdAltura.textContent;
+        if(this.validationPeso(peso, tdIMC, pacientes[i]) !== false && this.validationAltura(altura, tdIMC, pacientes[i]) !== false){
+          var imc              = peso / (Math.pow(altura,2));
+          tdIMC.textContent    = imc.toFixed(2);
+        }
+        
     }
-    
 }
 
 
-function validationPeso(peso, tdIMC){
+function validationPeso(peso, tdIMC,paciente){
     if(peso <= 0 || peso >= 700){ 
       tdIMC.textContent = 'Peso inválido: ' + peso + " kg";
-      return peso = false;
+      paciente.classList.add('paciente-invalido');      
     }else{
         return peso;
     }
 }
 
-function validationAltura(altura, tdIMC){
+function validationAltura(altura, tdIMC,paciente){
     if(altura <= 0 || altura >= 6.20){ 
       tdIMC.textContent = 'Altura inválida: ' + altura + " centímetros";
+      paciente.classList.add('paciente-invalido');
       return altura = false;
     }else{
         return altura;
